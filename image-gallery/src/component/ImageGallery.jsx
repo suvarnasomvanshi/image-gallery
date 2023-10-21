4e3ea01684a3ea132ddf016e4f6ca83de88f75a6
suvarna <somavanshisuvarna48@gmail.com>
2023-10-21 17:37:18 +0530

    creating search bar

--- image-gallery/src/component/ImageGallery.jsx
+++ image-gallery/src/component/ImageGallery.jsx
@@ -1,17 +1,17 @@
 import React, { useState } from 'react'
 import axios from "axios";
-
+import SearchBar from './SearchBar';
 const ImageGallery = () => {
 
     const[images,setImages] = useState([]);
-
+    const[searchValue,setSearchValue] = useState(' ');
 
     const API_URL = 'https://api.unsplash.com';
     const API_KEY = 'TE7HiDPxbXQWX9MqwdPv5dDGXSrz8Q93rXjDOJHaw7g';
 
 
     const fetchImages = async()=>{
-        
+
         try{
             const response = await axios.get(`${API_URL}/photos`,{
                 headers:{Authorization:`Client-Id ${API_KEY}`},
@@ -26,7 +26,10 @@ const ImageGallery = () => {
 
   return (
     <div>
-      
+      <SearchBar
+      searchValue = {searchValue}
+      handleChange ={(newVal)=>setSearchValue(newVal)}
+      />
     </div>
   )
 }
---
+++ image-gallery/src/component/SearchBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from "@mui/material/Button";
+
+const SearchBar = () => {
+  return (
+    <div>
+
+    <input
+    type='text'
+    placeholder='Search for images'
+    value={searchValue}
+    onChange={(e)=>handleChange(e.target.value)}
+    />
+
+    <Button variant='contained' color='primary' onClick={onSearch}>Search</Button>
+      
+    </div>
+  )
+}
+
+export default SearchBar
